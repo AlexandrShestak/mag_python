117a9ebc80e01df36f12a120f45a39bebe1beed1
AlexandrShestak <shestakalexandr@mail.ru>
2017-09-19 23:23:05 +0300

    Removed debug info

--- lab2/unique.py
+++ lab2/unique.py
@@ -7,10 +7,4 @@ def get_unique(collection):
     for it in combinations(collection, 2):
         unique[tuple(sorted(it))] += 1
 
-    for key, value in unique.iteritems():
-        print str(key) + '   ' + str(value)
-
     return unique
-
-
-print get_unique([2, 5, 7, 3, 5, 3])
