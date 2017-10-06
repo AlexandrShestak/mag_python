cb91fc428e845d5fd6f4720913107839dd88c69b
AlexandrShestak <shestakalexandr@mail.ru>
2017-10-06 12:08:55 +0300

    Replaced set with list

--- lab3/big_number.py
+++ lab3/big_number.py
@@ -5,11 +5,11 @@ def index(text, numbers, k=5):
     if isinstance(numbers, int):
         numbers = (numbers,)
 
-    positions = set()
+    positions = []
     total_occurrences = 0
     for number in numbers:
-        occurrences_set = set([matcher.start() + 1 for matcher in re.finditer(str(number), text)])
-        total_occurrences += occurrences_set.__len__()
-        positions.update(occurrences_set)
+        occurrences_list = [matcher.start() + 1 for matcher in re.finditer(str(number), text)]
+        total_occurrences += occurrences_list.__len__()
+        positions += occurrences_list
 
     return total_occurrences, sorted(positions)[:k]
